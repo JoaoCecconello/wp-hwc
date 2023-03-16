15347778eba4f356ca562bfceee9a4e8cd935d58
João Cecconello <65190540+JoaoCecconello@users.noreply.github.com>
2023-03-16 17:34:25 -0300

    Fixing my grep mistake

--- wp-cleaner.sh
+++ wp-cleaner.sh
@@ -34,9 +34,9 @@ if [ -f "$WP_VERSION_FILE_PATH" ]; then
     # loop para verificar todos os plugins, verifica se é diretório, depois encontra a versão e baixa o plugin novamente
     for f in ./wp-content/plugins/*; do
         if [ -d "$f" ]; then
-            echo "$pwd"
+            
             WP_PLUGIN_NAME="${f##*/}"
-            WP_PLUGIN_VERSION=`grep 'Version: ' $f/* | tr -d '[:alpha:][:space:][$_=:;\47]'`
+            WP_PLUGIN_VERSION=`grep -h 'Version: ' $f/* | tr -d '[:alpha:][:space:][$_=:;\47]'`
             printf "Downloading from: https://downloads.wordpress.org/plugin/$WP_PLUGIN_NAME.$WP_PLUGIN_VERSION.zip\n"
             wget --quiet -O "./wordpress/wp-content/plugins/$WP_PLUGIN_NAME.zip" "https://downloads.wordpress.org/plugin/$WP_PLUGIN_NAME.$WP_PLUGIN_VERSION.zip" \
                 && unzip -qq ./wordpress/wp-content/plugins/$WP_PLUGIN_NAME.zip \
